3e06e0a407c626e9ff65330dde865559c4df2b80
MarcosSantana1212 <mr.santana1212@gmail.com>
2022-01-13 20:44:00 -0400

    Navigation class added to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Navigation from './components/Navigation';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <a href="#" className='text-white'>
-            Tasks 
-          </a>
-        </nav>
+        <Navigation /> 
         <img src={logo} className="App-logo" alt="logo"></img>
       </div>
     )
